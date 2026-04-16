70b060013b6f5d23fbe0cb62edad16d0e29dcfaf
KingSadid <SadiSex07@gmail.com>
2026-04-16 14:44:54 -0500

    fixes in env

--- env/heritage.config.js
+++ env/heritage.config.js
@@ -1,9 +1,12 @@
-module.exports = {
+const heritageConfig = {
     host: 'localhost',
     user: 'root',
     password: 'root',
-    database: 'db_reservations',  
+    database: 'db_heritage',
+    port: 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 };
+
+module.exports = heritageConfig;
--- env/reservations.config.js
+++ env/reservations.config.js
@@ -1,9 +1,12 @@
-module.exports = {
+const reservationsConfig = {
     host: 'localhost',
     user: 'root',
     password: 'root',
-    database: 'db_heritage',  
+    database: 'db_reservations',
+    port: 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 };
+
+module.exports = reservationsConfig;
